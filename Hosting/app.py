44e399b207796ada8114bb05c9ba814acb36e7d3
Mihier Chopra <57339100+mihierchopra@users.noreply.github.com>
2024-04-12 18:32:55 -0600

    Update app.py

--- Hosting/app.py
+++ Hosting/app.py
@@ -70,7 +70,7 @@ ac= 1 if Active == 'Yes' else 0
 #input = np.array([Credit,Geography,Gender,Age,Tenure,Balance,Products,card,ac,Salary])
 input_data = {"Credit":Credit, "Geography": Geography,"Gender": Gender,"Age": Age,"Tenure": Tenure,"Balance":Balance,"Products":Products,"Credits":card,"Active":ac,"Salary":Salary}
 if st.button('Predict Churn'):
-    st.write(os.getcwd())
+        st.write(os.getcwd())
         with st.spinner('Predicting...'):
                 prediction = make_prediction(input_data)
         st.subheader("Prediction Result")
